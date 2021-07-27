426cdc3bd1193c5cbb003085349d998288194c1e
sylviaantons <sylvia@donchev.is>
2021-07-27 13:13:14 +0200

    search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css'
+import Dictionary from './Dictionary'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="Container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+      </div>
+      <footer>Coded by Sylvia</footer>
     </div>
   )
 }
