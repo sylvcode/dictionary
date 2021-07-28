713f11adffe051e2f64a576e08f0f60b6ef887f2
sylviaantons <sylvia@donchev.is>
2021-07-28 10:12:59 +0200

    axios

--- src/Dictionary.jsx
+++ src/Dictionary.jsx
@@ -1,16 +1,25 @@
 import React, { useState } from 'react'
 import './Dictionary.css'
+import axios from 'axios'
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState('')
 
+  function handleResponse(response) {
+    console.log(response.data)
+  }
+
   function search(event) {
     event.preventDefault()
-    alert(`Searching for ${keyword}`)
+    alert(`Searching for ${keyword} definition`)
+
+    //documnetation from dictionaryapi.dev //
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`
+    axios.get(apiUrl).then(handleResponse)
   }
 
   function handleKeywordChange(event) {
-    console.log(event.target.value)
     setKeyword(event.target.value)
   }
 
