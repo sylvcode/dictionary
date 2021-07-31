f45482d4249fea99eea69e84a059884327e32033
sylviaantons <sylvia@donchev.is>
2021-07-31 19:11:52 +0200

    Meanings

--- src/Dictionary.jsx
+++ src/Dictionary.jsx
@@ -1,17 +1,19 @@
 import React, { useState } from 'react'
 import './Dictionary.css'
 import axios from 'axios'
+import Results from './Results'
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState('')
+  let [results, setResults] = useState(null)
 
   function handleResponse(response) {
-    console.log(response.data)
+    console.log(response.data[0])
+    setResults(response.data[0])
   }
 
   function search(event) {
     event.preventDefault()
-    alert(`Searching for ${keyword} definition`)
 
     //documnetation from dictionaryapi.dev //
 
@@ -28,6 +30,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   )
 }
