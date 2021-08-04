117acc796eeea5051043117d8bc38dfc99b57d18
sylviaantons <sylvia@donchev.is>
2021-08-04 12:28:17 +0200

    design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css'
 import Dictionary from './Dictionary'
+import logo from './logo.png'
 
 function App() {
   return (
     <div className="App">
       <div className="Container">
-        <header className="App-header"></header>
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+
         <main>
           <Dictionary defaultKeyword="Sunshine" />
         </main>
